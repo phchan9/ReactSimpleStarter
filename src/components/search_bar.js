ac562b846026f768db32bb15c6b43b4afa3b1526
phchan9 <phckhc@gmail.com>
2017-05-05 17:58:02 +0800

    craft the arch of website and add some css

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -13,7 +13,6 @@ class SearchBar extends Component {
                     value={this.state.term}
                     onChange={(event) => this.setState({term: event.target.value})} />
                 <br />
-                Value of input: {this.state.term}
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 import { API_KEY } from './config';
 
-// Create a component
-const App = function() {
-    return (
-    <div>
-        <SearchBar />
-    </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+
+    render() {
+        return (
+            <div className="search-bar">
+              <SearchBar />
+              <VideoDetail video={this.state.selectedVideo} />
+              <VideoList
+                onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+                videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 // Take this component's generated html and put it on the page (dom)
